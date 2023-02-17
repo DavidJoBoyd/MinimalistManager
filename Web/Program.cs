1b89993624e44c381b4dccb86605ffba740d54c6
DavidJoBoyd <cw.davidboyd@gmail.com>
2023-02-16 19:28:14 -0600

    DataBase Started

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,14 +1,23 @@
+using Infastructure.DataAccess;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Web;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
+public sealed class Program
 {
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-await builder.Build().RunAsync();
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.Services.AddScoped(sp => new HttpClient
+        {
+            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        });
+        
+        await builder.Build().RunAsync();
+    }
+}
---
+++ src/Infastructure/DataAccess/MinimalistContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infastructure.DataAccess;
+
+public class MinimalistContext : DbContext
+{
+    public MinimalistContext()
+    {
+        
+    }
+    public MinimalistContext(DbContextOptions options) : base(options){}
+
+    public DbSet<Item> Items { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+        "data source=DESKTOP-824D8L0;initial catalog=Minimalist Manager;trusted_connection=true;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+    }
+}
